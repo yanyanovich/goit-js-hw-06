c0f13ba5d3512b324e8ba087563e571079d12bd8
yanyanovich <yan.tokaruk@gmail.com>
2023-02-05 21:57:46 +0200

    fix 6 task

--- js/task-06.js
+++ js/task-06.js
@@ -1,8 +1,15 @@
 const validationInput = document.querySelector("#validation-input");
 validationInput.addEventListener("blur", (e) => {
   if (validationInput.value.length === Number(validationInput.dataset.length)) {
-    validationInput.classList.remove("invalid");
-    return validationInput.classList.add("valid");
+    if (validationInput.classList.contains("invalid")) {
+      validationInput.classList.remove("invalid");
+    }
+    validationInput.classList.add("valid");
+  } else {
+    if (validationInput.classList.contains("valid")) {
+      validationInput.classList.remove("valid");
+    }
+    validationInput.classList.add("invalid");
   }
-  return validationInput.classList.add("invalid");
+  // return validationInput.classList.add("invalid");
 });
