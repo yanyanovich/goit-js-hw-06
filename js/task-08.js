ec4668a90116861e895837e6e26a5c5a7b1b54f5
yanyanovich <yan.tokaruk@gmail.com>
2023-02-05 21:34:53 +0200

    finish 8 task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,12 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const {
+    elements: { email, password },
+  } = e.currentTarget;
+  if (email.value === "" || password.value === "")
+    alert("Всі поля повинні бути заповнені");
+  console.log(`Login: ${email.value}, Password: ${password.value}`);
+  e.currentTarget.reset();
+});
