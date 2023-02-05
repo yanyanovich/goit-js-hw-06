1ea0416c98394cb65834fc40330181cb52d8ea77
yanyanovich <yan.tokaruk@gmail.com>
2023-02-05 19:54:14 +0200

    finish 2 task

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const ingredientsList = [];
+const ingredientsListEl = document.querySelector("#ingredients");
+for (let i = 0; i < ingredients.length; i++) {
+  const option = ingredients[i];
+  const ingredientsItems = document.createElement("li");
+  ingredientsItems.textContent = option;
+  ingredientsItems.className = "item";
+  ingredientsList.push(ingredientsItems);
+}
+ingredientsListEl.append(...ingredientsList);
