ce2b54806a87598643ad9eaf29f85e958b15a622
yanyanovich <yan.tokaruk@gmail.com>
2023-02-05 21:44:28 +0200

    finish 9 task

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const colorEl = document.querySelector(".color");
+
+const changeColorButton = document.querySelector(".change-color");
+
+changeColorButton.addEventListener("click", (e) => {
+  console.log(getRandomHexColor());
+  document.body.style.backgroundColor = getRandomHexColor();
+  colorEl.textContent = document.body.style.backgroundColor;
+});
